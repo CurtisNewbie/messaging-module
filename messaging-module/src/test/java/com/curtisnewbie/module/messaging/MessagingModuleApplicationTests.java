e6ca032c89eef5f6401f193d42c2882754223dc3
CurtisNewbie <zhuangyongj@gmail.com>
2021-08-19 18:24:09 +0800

    Init project

---
+++ messaging-module/src/test/java/com/curtisnewbie/module/messaging/MessagingModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curtisnewbie.module.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
